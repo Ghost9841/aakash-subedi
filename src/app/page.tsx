08a19754817fda8f0fc73637d9814ebbac424b99
Asis423 <ashishsubedi423@gmail.com>
2024-10-25 07:44:13 +0545

    Added Resume and Contact buttons on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import profilepic from './utilits/profile/developer-pic-1.png';
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { LinkArrow } from "./components/icons";
 
 export default function Home() {
   return (
@@ -14,18 +16,17 @@ export default function Home() {
         <Image  src={profilepic} alt="Aakash Pic"/>
         </motion.div>
       </div>
-      <div className="w-1/2 h-full flex flex-col items-center justify-center bg-light p-32">
+      <div className="w-1/2 h-full flex-shrink-0 items-center justify-center pt-0 bg-light p-32">
         <motion.div
-          className="text-6xl font-bold mb-4 capitalize"
+          className="text-6xl font-bold mb-6 capitalize"
           initial={{ opacity: 0, y: -20 }} 
           animate={{ opacity: 1, y: 0 }} 
           transition={{ duration: 0.5 }}
-          whileHover={{scale: 1}}
         >
           Crafting Digital Experiences with Code and Creativity
         </motion.div>
         <motion.p 
-          className="text-lg mb-2"
+          className="text-lg my-6"
           initial={{ opacity: 0, y: -20 }} 
           animate={{ opacity: 1, y: 0 }} 
           transition={{ duration: 0.6, delay: 0.2 }}
@@ -40,6 +41,25 @@ export default function Home() {
         >
           Dive into my projects and articles to see my journey and expertise in modern web technologies.
         </motion.p>
+        <div className="flex items-center self-start mt-4">
+        <Link href="/mycv.pdf" 
+        target={"_blank"}
+        className="flex items-center bg-dark text-light p-2.5 px-6
+        rounded-lg text-lg font-poppin font-semibold hover:bg-light hover:text-dark
+        border-2 border-solid border-transparent hover:border-dark"
+        download={true}
+        >
+           Resume
+        <LinkArrow className={"w-6 ml-1"}></LinkArrow>
+        </Link>
+
+        <Link href="asubedi778@gmail.com" 
+        target="_blank"
+        className="ml-4 text-lg font-poppin font-medium capitalize text-dark underline"
+        >
+          Contact
+          </Link>
+        </div>
       </div>
     </div>
   );
