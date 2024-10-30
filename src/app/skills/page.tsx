d613378c08eb2f234ef0eb87fd3ecb6064ee0d9d
Asis423 <asubedi778@gmail.com>
2024-10-30 19:29:51 +0545

    Started Skill Page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-const page = () => {
+const skills = () => {
   return (
     <div>
-      
+      <div className="text-7xl font-poppin justify-center flex font-semibold pt-8 my-2">Skills</div>
+      <div className=' items flex  justify-between pt-12'>
+        
+      <span className='text-3xl font-bold '>
+        Experience
+        </span><br/> <br/>
+      Technician 
+      2022-2024
+      JNICT Technologies Thankot, Nepal (On-site) <br/>
+      ```bash
+      Provided technical support and maintenance
+      </div>
+
     </div>
   )
 }
 
-export default page
+export default skills
